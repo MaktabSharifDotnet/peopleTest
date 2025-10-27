a213c6a090c931d564db16fdc001b1580616a600
aliizadjoo <Ali.zadjoo@gmail.com>
2025-10-27 17:00:25 +0330

    Initial commit

---
+++ src/UI_MVC/Controllers/PeopleController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UI_MVC.Models.DataBase;
+
+namespace UI_MVC.Controllers
+{
+    public class PeopleController : Controller
+    {
+        public IActionResult Index()
+        {
+            var dbContext = new AppDbContext();
+            var people = dbContext.People.ToList();
+
+            return View(people);
+        }
+    }
+}
