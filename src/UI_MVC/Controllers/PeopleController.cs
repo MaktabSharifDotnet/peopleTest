53b4fc8bd46f92dfada019f184b3c11921680f81
aliizadjoo <Ali.zadjoo@gmail.com>
2025-10-29 11:15:25 +0330

    final tuesday

--- src/UI_MVC/Controllers/PeopleController.cs
+++ src/UI_MVC/Controllers/PeopleController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using UI_MVC.Models.DataBase;
+using UI_MVC.Models.Entities;
 
 namespace UI_MVC.Controllers
 {
@@ -15,6 +16,43 @@ namespace UI_MVC.Controllers
         public IActionResult Add() 
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Add(Person model)
+        {
+            AppDbContext dbContext = new AppDbContext();
+            dbContext.People.Add(model);
+            dbContext.SaveChanges();
+            return RedirectToAction("index");
+            //return View();
+        }
+        public IActionResult Edit(int id)
+        {
+            AppDbContext dbContext = new AppDbContext();
+            var result=dbContext.People.Find(id);
+            return View(result);
+        }
+        [HttpPost]
+        public IActionResult Edit(Person model)
+        {
+            AppDbContext dbContext = new AppDbContext();
+            var result = dbContext.People.Find(model.Id);
+            result.FirstName = model.FirstName;
+            result.LastName = model.LastName;
+            result.CityName = model.CityName;
+            result.PhoneNumber = model.PhoneNumber;
+            dbContext.SaveChanges();
+            return RedirectToAction("index");
+            //return View();
+        }
+        [HttpPost]
+        public IActionResult Delete(int id) 
+        {
+            AppDbContext dbContext = new AppDbContext();
+            var result = dbContext.People.Find(id);
+            dbContext.People.Remove(result);
+            dbContext.SaveChanges();
+            return RedirectToAction("index");
         }
     }
 }
